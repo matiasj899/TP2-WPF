b76014cbb5cc0aec049b6a2d00fe6d5263b3e1f6
blocklal <lagoslautaro7@gmail.com>
2024-09-20 20:46:54 -0300

    'interfaz' creada

--- TP2-WPF/MainWindow.xaml.cs
+++ TP2-WPF/MainWindow.xaml.cs
@@ -16,8 +16,46 @@ namespace TP2_WPF;
 /// </summary>
 public partial class MainWindow : Window
 {
+    int contador = 0 ;
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void AgregarUsuario(object sender, RoutedEventArgs e)
+    {
+        var userBox = new Border
+        {
+            BorderBrush = Brushes.Black,
+            BorderThickness = new System.Windows.Thickness(1),
+            Margin = new System.Windows.Thickness(5),
+            Padding = new System.Windows.Thickness(5),
+            Child = new StackPanel
+            {
+                Children =
+                {
+                    new TextBlock { Text = "BUENAS"},
+                    new TextBlock { Text = "BUENAS"},
+                    new TextBlock { Text = "BUENAS"},
+
+                }
+            }
+        };
+        if (contador == 0)
+        {
+            PrimerPanel.Children.Add(userBox);
+            contador++;
+        }
+        else if (contador == 1)
+        {
+            SegundoPanel.Children.Add(userBox);
+            contador++;
+        }
+        else if(contador == 2)
+        {
+            TercerPanel.Children.Add(userBox);
+            contador = 0 ;
+        }
+
     }
 }
