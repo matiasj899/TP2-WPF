637828994fdf4a4d91614b1ce0f5859c0a86f262
Kelly <kelymeza@hotmail.com>
2024-10-08 19:49:42 -0300

    Void NotaFinal

--- TP2-WPF/AssemblyInfo.cs
+++ TP2-WPF/AssemblyInfo.cs
@@ -8,3 +8,8 @@ using System.Windows;
                                                 //(used if a resource is not found in the page,
                                                 // app, or any theme specific resource dictionaries)
 )]
+float NotaFinal(float parcial1, float parcial2)
+{
+    float notaFinal = (parcial1 + parcial2) / 2;
+    return notaFinal;
+}
